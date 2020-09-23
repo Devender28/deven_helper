03e6fb00ba378d3742b1cb56f36f34bca7ed34a9
Devender28 <55614295+Devender28@users.noreply.github.com>
2020-09-23 11:58:28 +0530

    Update deven_helper.php

--- deven_helper.php
+++ deven_helper.php
@@ -44,19 +44,19 @@ function do_upload($a,$b,$c)
 									}
 
 function create_thumb($filename,$width,$height)									
-									{
-										$CI =& get_instance();
-									$config['source_image'] = APPPATH.'../uploads/user/'.$filename;
-									$config['create_thumb'] = TRUE;
-									$config['maintain_ratio'] = TRUE;
-									$config['width']         = $width;
-									$config['height']       = $height;
-		
-									$CI->load->library('image_lib', $config);
-		
-									$CI->image_lib->resize();									
+			{
+				$CI =& get_instance();
+			$config['source_image'] = APPPATH.'../uploads/user/'.$filename;
+			$config['create_thumb'] = TRUE;
+			$config['maintain_ratio'] = TRUE;
+			$config['width']         = $width;
+			$config['height']       = $height;
 
-									}
+			$CI->load->library('image_lib', $config);
+
+			$CI->image_lib->resize();									
+
+			}
 
 
 
